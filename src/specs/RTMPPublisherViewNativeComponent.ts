6a625a8696b81c595a098bf9640d824040427b26
huytran125 <64766573+huytran125@users.noreply.github.com>
2025-10-31 00:00:48 +0700

    Update RTMPPublisherViewNativeComponent.ts

--- src/specs/RTMPPublisherViewNativeComponent.ts
+++ src/specs/RTMPPublisherViewNativeComponent.ts
@@ -7,13 +7,13 @@ import type { DirectEventHandler } from 'react-native/Libraries/Types/CodegenTyp
 export interface NativeProps extends ViewProps {
   streamURL: string;
   streamName: string;
-  onConnectionFailed?: DirectEventHandler<{ data: string }>;
-  onConnectionStarted?: DirectEventHandler<{ data: string }>;
-  onConnectionSuccess?: DirectEventHandler<{ data: null }>;
-  onDisconnect?: DirectEventHandler<{ data: null }>;
-  onNewBitrateReceived?: DirectEventHandler<{ data: number }>;
-  onStreamStateChanged?: DirectEventHandler<{ data: string }>;
-  onBluetoothDeviceStatusChanged?: DirectEventHandler<{ data: string }>;
+  onConnectionFailed?: DirectEventHandler<{ data?: string }>;
+  onConnectionStarted?: DirectEventHandler<{ data?: string }>;
+  onConnectionSuccess?: DirectEventHandler<{ data?: string }>;
+  onDisconnect?: DirectEventHandler<{ data?: string }>;
+  onNewBitrateReceived?: DirectEventHandler<{ data?: number }>;
+  onStreamStateChanged?: DirectEventHandler<{ data?: string }>;
+  onBluetoothDeviceStatusChanged?: DirectEventHandler<{ data?: string }>;
 }
 
 // @ts-ignore - codegenNativeComponent is only available in RN 0.68+
